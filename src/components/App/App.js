07aeb80a5bbfbbadae381436345fd4f140964947
Nick Hart <nick.hart.des@gmail.com>
2020-09-22 09:13:47 -0600

    Add state to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,10 +7,12 @@ import OrderForm from '../../components/OrderForm/OrderForm';
 class App extends Component {
   constructor(props) {
     super();
+    this.state = {};
   }
 
   componentDidMount() {
     getOrders()
+      .then(orders => this.setState({ orders }))
       .catch(err => console.error('Error fetching:', err));
   }
 
